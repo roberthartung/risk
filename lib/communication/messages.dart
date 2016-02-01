c160b06048092dbe73847d5e2abe7187aa8d525c
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2016-02-01 10:53:21 +0100

    Removed usage of runtimeType -> This makes problems in dart2js

--- lib/communication/messages.dart
+++ lib/communication/messages.dart
@@ -1,8 +1,10 @@
+//import 'dart:mirrors';
 import 'package:risk/world/world.dart';
 import 'package:risk/user.dart';
 import 'package:risk/game.dart' show GameState;
 
 class Message {
+  String get type;
   Message() {
 
   }
@@ -12,12 +14,14 @@ class Message {
   }
 
   Map toObject() {
-    return {'type':this.runtimeType.toString()};
+    return {'type':type};
   }
 }
 
 class StartGameMessage extends Message {
   bool random;
+
+  String get type => 'StartGameMessage';
 
   StartGameMessage(this.random) : super();
 
@@ -29,27 +33,33 @@ class StartGameMessage extends Message {
 }
 
 class NextMoveMessage extends Message {
+  String get type => 'NextMoveMessage';
   NextMoveMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class ConquerMoveMessage extends NextMoveMessage {
+  String get type => 'ConquerMoveMessage';
   ConquerMoveMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class ReinforceMoveMessage extends NextMoveMessage {
+  String get type => 'ReinforceMoveMessage';
   ReinforceMoveMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class MoveFinishedMessage extends Message {
+  String get type => 'MoveFinishedMessage';
   MoveFinishedMessage() : super();
 }
 
 class GameStateChangedMessage extends Message {
+  String get type => 'GameStateChangedMessage';
   final GameState state;
   GameStateChangedMessage.fromObject(obj) : this.state = GameState.values[obj['state']];
 }
 
 class LoginMessage extends Message {
+  String get type => 'LoginMessage';
   final String name;
   final String pass;
   final String game;
@@ -66,6 +76,7 @@ class LoginMessage extends Message {
   }
 }
 class GameInformationMessage extends Message {
+  String get type => 'GameInformationMessage';
   final GameState state;
   final String leader;
   final User me;
@@ -75,6 +86,7 @@ class GameInformationMessage extends Message {
 }
 
 class ListOfUsersMessage extends Message {
+  String get type => 'ListOfUsersMessage';
   final List<User> users;
   ListOfUsersMessage.fromObject(obj) : super.fromObject(obj), users = _unpackUsers(obj);
 
@@ -90,6 +102,7 @@ class ListOfUsersMessage extends Message {
 }
 
 class UserMessage extends Message {
+  String get type => 'UserMessage';
   final User user;
   UserMessage(this.user);
 
@@ -107,27 +120,33 @@ class UserMessage extends Message {
 }
 
 class UserJoinedMessage extends UserMessage {
+  String get type => 'UserJoinedMessage';
   UserJoinedMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class UserQuitMessage extends UserMessage {
+  String get type => 'UserQuitMessage';
   UserQuitMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class UserOfflineMessage extends UserMessage {
+  String get type => 'UserOfflineMessage';
   UserOfflineMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class UserOnlineMessage extends UserMessage {
+  String get type => 'UserOnlineMessage';
   UserOnlineMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class LeaderChangedMessage extends Message {
+  String get type => 'LeaderChangedMessage';
   final String leader;
   LeaderChangedMessage.fromObject(obj) : super.fromObject(obj), this.leader = obj['leader'];
 }
 
 class CountryMessage extends Message {
+  String get type => 'CountryMessage';
   final Country country;
   CountryMessage(this.country) : super();
 
@@ -141,24 +160,28 @@ class CountryMessage extends Message {
 }
 
 class ConquerMoveFinishedMessage extends CountryMessage {
+  String get type => 'ConquerMoveFinishedMessage';
   ConquerMoveFinishedMessage(country) : super(country);
 }
 
 class ReinforceMoveFinishedMessage extends CountryMessage {
+  String get type => 'ReinforceMoveFinishedMessage';
   ReinforceMoveFinishedMessage(country) : super(country);
 }
 
-
 class CountryConqueredMessage extends CountryMessage {
+  String get type => 'CountryConqueredMessage';
   final User user;
   CountryConqueredMessage.fromObject(obj) : super.fromObject(obj), this.user = new User(obj['user']['name']);
 }
 
 class CountryReinforcedMessage extends CountryMessage {
+  String get type => 'CountryReinforcedMessage';
   CountryReinforcedMessage.fromObject(obj) : super.fromObject(obj);
 }
 
 class CountriesListMessage extends Message {
+  String get type => 'CountriesListMessage';
   final Map countries;
   CountriesListMessage.fromObject(obj) : super.fromObject(obj), this.countries = obj['countries'] {
     countries.forEach((String id, Map data) {
